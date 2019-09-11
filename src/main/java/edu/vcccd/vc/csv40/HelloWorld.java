29f8273d61768276cacc6553962af23836e394c0
Gabriel Martin <54370901+kooshades25@users.noreply.github.com>
2019-09-11 16:33:00 -0700

    Create HelloWorld.java

---
+++ src/main/java/edu/vcccd/vc/csv40/HelloWorld.java
@@ -0,0 +1,18 @@
+package edu.vcccd.vc.csv40;
+
+/**
+ * CS V40 Beginning Java
+ * Assignment: edu.vcccd.vc.csv40.HelloWorld
+ *
+ * Statement of code ownership: I hereby state that I have written all of this
+ * code and I have not copied this code from any other person or source.
+ *
+ * @author [none of your business jk it's gabe]
+ */
+public class HelloWorld {
+        //hello world is nothing but a carousel of agony please help me
+
+public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        }
+        }
